3a7e355830455824f8e9b332b565517903d07369
rahul <rahul@rptdev.onmicrosoft.com>
2022-06-04 22:23:17 +0530

    Add a script for site collection app catalog

---
+++ App Catalog.ps1
@@ -0,0 +1,4 @@
+﻿# Script to enable app catalog at site collection level
+Connect-SPOService
+$site = Get-SPOSite https://rptdev.sharepoint.com/sites/SRT
+Add-SPOSiteCollectionAppCatalog -Site $site
